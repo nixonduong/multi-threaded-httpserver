d485310e13ea1bef31ee5379865818932d55931b
Nixon Duong <niduong@ucsc.edu>
2019-10-25 20:14:18 -0700

    first commit

---
+++ asgn1/httpserver.cpp
@@ -0,0 +1,14 @@
+#include <sys/socket.h>
+
+int main(int argc, char** argv){
+    if (argc == 1) {
+
+    } else if (argc == 2){
+
+    } else if (argc == 3){
+
+    } else {
+
+    }
+    return 0;
+}
