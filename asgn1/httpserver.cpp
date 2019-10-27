619dcd5830484cbadebfd5822ddbb395806d16b7
Nixon Duong <niduong@ucsc.edu>
2019-10-27 13:43:10 -0700

    parse filename for get request

--- asgn1/httpserver.cpp
+++ asgn1/httpserver.cpp
@@ -9,11 +9,10 @@ Dependencies: None
 Description: 
 */
 
-
 #include <sys/socket.h>
 #include <stdio.h>
 #include <netdb.h>
-
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
@@ -21,37 +20,45 @@ Description:
 #include <err.h>
 #include <string.h>
 
-
 #define BUFFER_SIZE 32768
 #define BACKLOG_SIZE 16
 
 uint8_t createNetworkSocket(char* hostname, char* port);
 void processSocketConenctions(uint8_t main_socket, struct addrinfo *addrs);
-void parseRequest(char* buffer, ssize_t bytesRecv);
-void handleGetRequest();
-void handlePutRequest();
+void parseRequest(char* buffer, ssize_t bytesRecv, uint8_t sockfd);
+void handleGetRequest(char* buffer, uint8_t sockfd);
+void handlePutRequest(char* buffer, uint8_t sockfd);
 
-void handlePutRequest() {
-
-}
-
-void handleGetRequest() {
-
-}
-
-void parseRequest(char* buffer, ssize_t bytesRecv) {
+void handlePutRequest(char* buffer, uint8_t sockfd) {
     char* token;
     token = strtok(buffer, " ");
-    printf("%s\n", token);
+    printf("%s\n", "PUT");
+}
+
+void handleGetRequest(char* buffer, uint8_t sockfd) {
+    char filename[27];
+    sscanf(buffer, "%*s /%s", filename);
+    printf("%s\n", filename);
+}
+
+void parseRequest(char* buffer, ssize_t bytesRecv, uint8_t sockfd) {
+    char type[3];
+    sscanf(buffer, "%s", type);
+    if (strcmp(type, "GET")) {
+        handleGetRequest(buffer, sockfd);
+    }
+    if (strcmp(type, "PUT")) {
+        handleGetRequest(buffer, sockfd);
+    }
 }
 
 void processSocketConenctions(uint8_t main_socket, struct addrinfo *addrs) {
     char buffer[BUFFER_SIZE];
     uint8_t sockfd = 1;
-    while(sockfd) {
+    while(1) {
         sockfd = accept(main_socket, NULL, NULL);
         ssize_t bytesRecv = recv(sockfd, buffer, sizeof(buffer), 0);
-        parseRequest(buffer, bytesRecv);
+        parseRequest(buffer, bytesRecv, sockfd);
         // resolve request
     }
 }
@@ -69,10 +76,11 @@ uint8_t createNetworkSocket(char* hostname, char* port, struct addrinfo *addrs, 
 }
 
 int main(int argc, char** argv) {
-    char* hostname;
-    char* port; 
+    char* hostname = nullptr;
+    char* port = nullptr;
     if (argc == 1) {
         fprintf(stderr, "Error: 0 commandline arguments given, 1 required\n");
+        exit(1);
     } else if (argc == 2){
         hostname = argv[1];
         port = (char*)"80";
@@ -81,6 +89,7 @@ int main(int argc, char** argv) {
         port = argv[2];
     } else {
         fprintf(stderr, "Error: %d commandline arguments given, 1 required\n", argc);
+        exit(1);
     }
     if (hostname && port) {
         struct addrinfo *addrs, hints = {};
